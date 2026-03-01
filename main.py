f42891acb75a337b7797e8de6f0bc18f510793cf
Aman Mishra <amanmishra291203@gmail.com>
2026-03-02 02:27:54 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from app.camera import capture_frame
-from app.model import predict_density
-from app.controller import control_traffic
-
-while True:
-    img_path = capture_frame()  # capture frame.jpg
-    density = predict_density(img_path)  # Light / Moderate / Heavy
-    control_traffic(density)  # Adjust GPIO or simulate
-    
